dbad86f93e749f7bfe425ced60f83bb7963cded3
Andy Bettisworth <andy@accreu.com>
2019-09-03 14:24:15 -0500

    experimenting with arguments

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(a, b):
+  return a + b
 
 print(f1(1, 2))
 
@@ -12,6 +14,11 @@ print(f1(1, 2))
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(farg, *args):
+  if type(farg) is int:
+    return sum(args) + farg
+  else:
+    return sum(farg)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -28,6 +35,11 @@ print(f2(a))    # Should print 22
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(a, b=None):
+  if b == None:
+    return a + 1
+  else:
+    return a + b
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,6 +54,13 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(farg=None, **kwargs):
+  if farg is dict:
+    for k,v in farg.items():
+      print(f"key: {k}, value: {v}")
+  else:
+    for k,v in kwargs.items():
+      print(f"key: {k}, value: {v}")
 
 # Should print
 # key: a, value: 12
